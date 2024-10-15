7ff6478150c49dbd498df2c975a9532cc72aff20
sailer2002 <131645241+sailer2002@users.noreply.github.com>
2024-10-15 10:05:41 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.include_router(router)
 # 这个条件语句确保在本地运行时才执行uvicorn.run()
 # Vercel会自动处理应用的运行，不需要这部分代码
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app)
 
 # 为Vercel部署添加这一行
 # Vercel需要一个名为"app"的变量作为入口点
